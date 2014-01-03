babe2355c215961af57ffa4bd14ff40e45495cca
dvano <dvano@mail.ua>
2014-01-03 12:29:22 +0200

    Create Textures.java

---
+++ src/engine/game/Textures.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.engine.game;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author dvano
+ */
+public final class Textures
+{
+    public static final List<Image> textures = new ArrayList<>();
+
+    private Textures()
+    {
+    }
+}
